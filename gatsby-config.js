660468a92e5a59af41e984a190b6e182f58e9be0
Jagadeesh Palaniappan <jagtechgeek@gmail.com>
2018-10-16 11:15:51 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@
 'use strict';
 
 module.exports = {
+  pathPrefix: "/reactjs",
   siteMetadata: {
     title: 'React: A JavaScript library for building user interfaces',
     siteUrl: 'https://reactjs.org',
